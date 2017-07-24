e5a70b4bad895c2318253eaf65fe6e5e07d9ec49
Marty VanHoof <garchuvius@yahoo.ca>
2017-07-24 13:21:04 -0400

    Add first 3 functions to wikitable_parser.py

--- wikitable_parser.py
+++ wikitable_parser.py
@@ -6,3 +6,62 @@ import requests
 
 # put the URL for the site here
 URL = 'https://en.wikipedia.org/wiki/List_of_countries_by_past_and_future_population'
+
+def get_table_html(url):
+    '''Parse the html on the webpage and get the html corresponding
+    to the tables that we want.'''
+    
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, 'lxml')
+    tables = [t for t in soup.find_all('table', 'sortable wikitable')]
+    
+    return tables
+    
+tbls_html = get_table_html(URL)
+
+def coerce_to_int(s):
+    '''Coerce string to integer.'''
+    
+    try:
+        return int(s)
+    except ValueError:
+        return s
+
+def parse_table(table_html):
+    '''Parse the html data from a table into a list of Python dictionaries.
+    Each dict represents a row of the table data, where the dict keys are 
+    the column names.'''
+    
+    # extract the table headers text from the <th> tags and coerce them to ints
+    headers = [coerce_to_int( th.text ) for th in table_html.find('tr') if th != '\n']
+    # extract the table rows from the <tr> tags
+    rows_html = table_html.find_all('tr')[1:]
+    
+    # table_data will be populated and returned as a list of dictionaries
+    table_data = []
+    for row in rows_html:
+        row_dict = defaultdict()
+        # for each row, make a list of data values from the <td> tags
+        row_list = [td.text for td in row.find_all('td')]
+        # loop over the headers and populate row_dict with the relevant data
+        for i, header in enumerate(headers):
+            row_dict[ headers[i] ] = row_list[i].replace(u'\xa0', u'')
+            
+        table_data.append(row_dict)
+        
+    # remove the '%' keys and values
+    for row in table_data:
+        for key in list(row):
+            if key == '%':
+                row.pop(key)
+                
+    # Python doesn't like numbers with commas in them
+    for row in table_data:
+        for key in row.keys():
+            row[key] = row[key].replace(',', '')
+            row[key] = coerce_to_int(row[key])
+        
+    return table_data
+        
+tbl_data = parse_table(tbls_html[0])
+print( tbl_data[0] )
