eb951cbd0617cbc40a40174e31082a1f5b9834f2
Marty VanHoof <garchuvius@yahoo.ca>
2017-10-13 16:54:29 -0400

    Make http request and get first link

--- wiki_web_crawl.py
+++ wiki_web_crawl.py
@@ -1,5 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
 
-def continue_crawl(search_history, target_url):
+def keep_crawling(search_history, target_url):
     '''decide whether to continue to crawl each webpage based on conditions.
     Returns False to stop crawling, otherwise the function returns True.'''
 
@@ -14,3 +16,20 @@ def continue_crawl(search_history, target_url):
         return False
     else:
         return True
+
+article_history = ['https://en.wikipedia.org/wiki/Machine_learning']
+target_url = 'https://en.wikipedia.org/wiki/Philosophy'
+
+while keep_crawling(article_history, target_url):
+    
+    # make http request to get the html from the last page in article_history
+    # and then make the html string into a BeautifulSoup object
+    r = requests.get(article_history[-1])
+    html = r.text
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    # get the first link from the article
+    a_tags = soup.p.find_all('a')
+    first_link = 'https://en.wikipedia.org' + a_tags[0].get('href') 
+    print(first_link)
+    break
